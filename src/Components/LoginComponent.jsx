c8179436974880d710bf41f20adcb35d5b9ad5ac
xMurungi <murungij9@.com>
2024-01-03 17:50:25 +0300

    UI Login Update

--- src/Components/LoginComponent.jsx
+++ src/Components/LoginComponent.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import "../Sass/LoginComponent.scss"; 
-import { loginAPI } from '../API/AuthAPI';
+import LinkedinLogo from "../Assets/LinkedinLogo.png";
+import { loginAPI } from "../API/AuthAPI";
+import "../Sass/LoginComponent.scss";
 
 export default function LoginComponent() {
   const login = () => {
@@ -8,13 +9,20 @@ export default function LoginComponent() {
   }
 
   return (
-    <div className="login"> 
-      <div className="header">Welcome to LinkedIn </div>
-      <form action="">
-        <input type="text" name='email' className="email-input" autoComplete='username' placeholder='Email' required />
-        <input type='password' name='password' className="password-input" autoComplete='current-password' placeholder='Password' required />
-        <button onClick={login} className="login-btn">Login</button>
-      </form>
+    <div className='box'>
+      <img src={LinkedinLogo} alt="Logo" className='LinkedinLogo' />
+      <div className="login">
+        <h1 className="header">Sign in</h1>
+        <p className='sub-heading'>Stay updated on your professional world</p>
+        <form action="">
+          <input type="text" name='email' className="email-input" autoComplete='username' placeholder='Email or Phone' required />
+          <input type='password' name='password' className="password-input" autoComplete='current-password' placeholder='Password' required />
+          <button onClick={login} className="login-btn">Login</button>
+        </form>
+        <div className='or' >or</div>
+        <button className='Googlebtn'> <span><img src={LinkedinLogo} alt="logo" /></span> Continue with Google</button>
+      </div>
+      <a target="_blank" href="https://icons8.com/icon/114445/linkedin-circled">LinkedIn Circled</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
     </div>
   )
 }
